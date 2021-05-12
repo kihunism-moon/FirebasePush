a7b021c11933c8d4cdf089a169da9170abd87d71
kihunism-moon <ymun893@gmail.com>
2021-05-13 00:20:21 +0900

    FCM -> Firebase Cloud Messaging debugger point

--- app/src/main/java/com/example/firebasepush/MainActivity.kt
+++ app/src/main/java/com/example/firebasepush/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.firebasepush
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+
+    private val resultTextView: TextView by lazy {
+        findViewById<TextView>(R.id.resultTextView)
+    }
+
+    private val firebaseToken: TextView by lazy {
+        findViewById<TextView>(R.id.firebaseTokenTextView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFirebase()
+
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if(task.isSuccessful) {
+                val token = task.result
+                firebaseToken.text = task.result
+            }
+        }
+
+
+
     }
 }
